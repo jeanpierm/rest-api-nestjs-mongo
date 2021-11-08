2529f11a53ed576a3436f051621b0b6c46e95ca3
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-11-07 20:24:52 -0500

    implement swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger OpenAPI config
+  const config = new DocumentBuilder()
+    .setTitle('NestJS RESTful API example')
+    .setDescription('The NestJS API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // validation pipe
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
